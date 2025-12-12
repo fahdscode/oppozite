99720779474d851018fb7b86a3d5a4ab6fd555de
fahdgamad080 <2201129@horus.edu.eg>
2025-12-12 13:42:13 +0200

    style: display MagneticCursor only on large screens.

--- src/components/ui/MagneticCursor.tsx
+++ src/components/ui/MagneticCursor.tsx
@@ -43,7 +43,7 @@ export const MagneticCursor = () => {
 
     return (
         <motion.div
-            className="fixed top-0 left-0 w-8 h-8 bg-white rounded-full pointer-events-none z-[9999] mix-blend-difference"
+            className="hidden lg:block fixed top-0 left-0 w-8 h-8 bg-white rounded-full pointer-events-none z-[9999] mix-blend-difference"
             style={{
                 translateX: cursorXSpring,
                 translateY: cursorYSpring,
